57ee161ae57d2d396b7aa1a63ff058e901641520
DHTri <145561569+DHTri@users.noreply.github.com>
2024-07-05 12:03:17 +0700

    Create Bai17.cpp

---
+++ Bai17.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long n;
+  cin>>n;
+  long long csc = 0, csl = 0;
+  while(n>0)
+  {
+    if((n%10) % 2 ==0 )
+      csc ++;
+    else  
+      csl ++;
+    n/=10;
+  }
+  if( csc == csl )
+  {
+    cout<<"yes";
+  }
+  else
+  {
+    cout<<"no";
+  }
+} 
